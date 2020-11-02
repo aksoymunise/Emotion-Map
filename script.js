4ce6857c10a3204bc7099bcc01e6951d6f6c81fa
Glitch (hello-webpage) <none>
2020-11-02 00:00:43 +0000

    🏍🚉 Checkpoint ./script.js:171012/2484 ./index.html:171012/517 ./style.css:171012/71

--- script.js
+++ script.js
@@ -4,72 +4,40 @@
 var shareddatabase = firebase.database();
 
 
-// get the current age and adds a number to it.
-function addAge(number) {
-  
-  // get the current Age as displayed on the website, and try to convert it to a number!
-  var currentAge = parseInt($("#currentAge").text());
-  
-  // after trying to convert it, if currentAge is not a number... then let's just set it to zero
-  // (isNaN is a special function that checks if an object is a number or not)
-  if(isNaN(currentAge)) { currentAge = 0; }
-  
-
-  // set our age in the shared database!
-  shareddatabase.ref("ourage").set({
-    age: currentAge + number
-  });
-  
-}
-
-
+var allfeelings = [];
 
 
 $(document).ready(function() {
+  
+  
+  
+  $("#submit").click(function() {
+  
+    var thisfeeling = {
+      name: $("#nameInput").val(),
+      feeling: $("#feelingInput").val(),
+      date: Date()
+    }
+    
+    allfeelings.push(thisfeeling)
+    
+    shareddatabase.ref("mvkc-log-test").set(allfeelings);
+  
+    
+    
+  })
 
-  
-  // when we click on it, change teh database
-  $("#setName").click(function() {
-    //alert("what");
-    
-    // from the database, find the reference(key/lookup code) of "ourname" (this is created arbitrarily)
-    // and set the 'name' of it to $("#nameInput").val(), which the contents of the input.
- 
-    shareddatabase.ref("ourname").set({
-      name: $("#nameInput").val()
-    });
-    
-  });
-  
-  
 
   // when the database changes, change the website  
-  shareddatabase.ref("ourname").on("value", function(snapshot) {
-    $("#currentName").text(snapshot.val().name);
+  shareddatabase.ref("mvkc-log-test").on("value", function(snapshot) {
     
-    console.log( snapshot.val() );
+    allfeelings = snapshot.val();
+    
+    console.log(allfeelings);
     
   });
   
   
 
-  
-  
-  
-  
-  $("#addAge").click(function() {
-    addAge(100);
-  })
-
-  $("#subtractAge").click(function() {
-    addAge(-100);
-  })
-
-  
-  shareddatabase.ref("ourage").on("value", function(snapshot) {
-    $("#currentAge").text(snapshot.val().age);
-  });
-  
-  
   
 });
